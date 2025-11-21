b6726649ec98febfd2a8be9fa4d661db1c416a81
huynh nguyen <huynhnv2@bap.jp>
2025-11-21 16:46:28 +0700

    create db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './modules/users/users.module';
+import { RestaurantsModule } from './modules/restaurants/restaurants.module';
+import { LikesModule } from './modules/likes/likes.module';
+import { MenuItemOptionsModule } from './modules/menu-item-options/menu-item-options.module';
+import { MenuItemsModule } from './modules/menu-items/menu-items.module';
+import { MenusModule } from './modules/menus/menus.module';
+import { OrderDetailModule } from './modules/order-detail/order-detail.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { ReviewsModule } from './modules/reviews/reviews.module';
 
 @Module({
   imports: [
+    UsersModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
@@ -13,7 +23,15 @@ import { MongooseModule } from '@nestjs/mongoose';
         uri: configService.get<string>('MONGODB_URI'),
       }),
       inject: [ConfigService],
-    })
+    }),
+    RestaurantsModule,
+    LikesModule,
+    MenuItemOptionsModule,
+    MenuItemsModule,
+    MenusModule,
+    OrderDetailModule,
+    OrdersModule,
+    ReviewsModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/users/schemas/user.schema.ts
+++ src/modules/users/schemas/user.schema.ts
@@ -1,1 +1,42 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import type { Date, HydratedDocument } from "mongoose";
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({timestamps: true})
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+  
+  @Prop()
+  password: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  image: string;
+  
+  @Prop({default: 'LOCAL'})
+  accountType: string;
+
+  @Prop({default: 'USERS'})
+  role: string;
+
+  @Prop({default: false})
+  isActive: boolean;
+  
+  @Prop()
+  codeId: string;
+
+  @Prop({ type: Date, required: true })
+  codeExpired: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
