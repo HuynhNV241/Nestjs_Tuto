4599e6d99bbd56e1e1d1fa4baf6773cdee6b905a
huynh nguyen <huynhnv2@bap.jp>
2025-12-04 10:36:48 +0700

    login jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
     private jwtService: JwtService
   ) {}
 
-  async signIn(username: string, pass: string): Promise<any> {
+  async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
     if (!user) {
       throw new UnauthorizedException();
@@ -19,10 +19,13 @@ export class AuthService {
     if (!isValidPassword) {
       throw new UnauthorizedException();
     }
-    const payload = { sub: user._id, username: user.email };
+    return user;
+  }
 
+  async login(user: any) {
+    const payload = { username: user.email, sub: user._id };
     return {
-      access_token: await this.jwtService.signAsync(payload),
+      access_token: this.jwtService.sign(payload),
     };
   }
 }
